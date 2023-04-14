97b382728999cf3685fa6def3cbc1ac2a382de07
Danilo Madrigalejos <dan@yahoo.com>
2023-04-14 23:59:38 +0800

    Pseudocode1

---
+++ test.py
@@ -0,0 +1,10 @@
+# Pseudocode
+# ask user for input
+# check each character 
+#   if a, change to *
+#   if e, change to &
+#   if i, change to #
+#   if o, change to +
+#   if u, change to !
+# print ouput
+s
